0e51ac7bc3381753a985fc59379f733e087cd8d6
Ilya M. <wuvuxd@proton.me>
2025-04-27 01:50:45 +0700

    chore: remove nuxt.studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,12 +16,6 @@ export default defineNuxtConfig({
 
   ssr: true,
 
-  content: {
-    preview: {
-      api: "https://api.nuxt.studio",
-    },
-  },
-
   googleFonts: {
     families: {
       Inter: [400, 500, 600, 700],
